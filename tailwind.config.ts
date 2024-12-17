d07e2a2c64ac3dfcb3c2aa65b16a0e68ffe5c36d
danielft2 <danielalmeida808z@gmail.com>
2024-12-17 20:22:55 -0300

    feat: files initials

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,30 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{ts,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: {
+          50:  "#FFF9ED",
+          100: "#FFF2D5",
+          200: "#FFE1A9",
+          300: "#FECB73",
+          400: "#FDAA3A",
+          500: "#FB8F14",
+          600: "#E8710A",
+          700: "#C4570A",
+          800: "#9B4411",
+          900: "#7D3A11",
+          950: "#431B07"
+        }
+      },
+      fontFamily: {
+        heading: ['var(--font-work-sans)'],
+        body: ['var(--font-raleway)'],
       },
     },
   },
