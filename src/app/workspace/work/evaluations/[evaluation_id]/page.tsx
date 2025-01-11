f8b67bbc557d1778d709811cea1a7337aa16001c
danielft2 <danielalmeida808z@gmail.com>
2025-01-11 07:34:01 -0300

    feat: add temporary fallback in evaluation details

--- src/app/workspace/work/evaluations/[evaluation_id]/page.tsx
+++ src/app/workspace/work/evaluations/[evaluation_id]/page.tsx
@@ -1,36 +1,24 @@
 import { Divider } from "@/components/ui/divider";
-import { retrieveEvaluationDetails } from "@/features/work/automatic-evaluations/service/retrieve-evaluation-details";
+import { EvaluateModelsList } from "@/features/work/automatic-evaluations/components/evaluation-details/evaluate-models-list";
 import { EvaluationDetailsHeader } from "@/features/work/automatic-evaluations/components/evaluation-details/evaluation-details-header";
-import { EvaluateModelCard } from "@/features/work/automatic-evaluations/components/evaluation-details/evaluate-model-card";
+import { Suspense } from "react";
 
 type Params = {
   evaluation_id: string;
 };
 
-export default async function EvaluationDetailsPage({
-  params,
-}: {
+export default async function EvaluationDetailsPage({ params }: {
   params: Params;
 }) {
   const { evaluation_id } = await params;
-  const details = await retrieveEvaluationDetails(evaluation_id);
-  const avaliableForEvaluation = !!details.data?.evaluation.filename_test;
 
   return (
     <>
       <EvaluationDetailsHeader />
-
       <Divider />
-
-      <section className="mt-8 flex flex-wrap gap-4">
-        {details.data?.models.map((model) => (
-          <EvaluateModelCard
-            key={model.id}
-            model={model}
-            isAvaliableForEvaluation={avaliableForEvaluation}
-          />
-        ))}
-      </section>
+      <Suspense fallback={<span>Carregando...</span>}>
+        <EvaluateModelsList evaluationId={evaluation_id} />
+      </Suspense>
     </>
   );
 }
