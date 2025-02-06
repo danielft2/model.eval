b945d721ff26304362b27e49b45459d787a2c208
danielft2 <danielalmeida808z@gmail.com>
2025-02-06 09:23:52 -0300

    fix: adjusts in form style

--- src/features/form/components/form-wizard/steps/welcome-step.tsx
+++ src/features/form/components/form-wizard/steps/welcome-step.tsx
@@ -11,7 +11,7 @@ export function WelcomeStep({ instructions }: WelcomeStepProps) {
 
   return (
     <div className="flex flex-col items-center h-full justify-center">
-      <div className="max-w-[600px] flex flex-col items-center gap-8">
+      <div className="max-w-[800px] flex flex-col items-center gap-8">
         <Image src="/welcome-form.svg" alt="ilustração bem vindo" width={200} height={200} />
         <div className="text-center space-y-2">
           <h1 className="text-slate-950 font-heading -tracking-wider font-medium text-4xl">
