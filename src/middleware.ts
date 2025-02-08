877f55b7af59c99aced7b3092bfca3e777143a86
danielft2 <danielalmeida808z@gmail.com>
2025-02-08 10:17:07 -0300

    fix: middlaware

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,6 +44,6 @@ export async function middleware(request: NextRequest) {
  
 export const config = {
   matcher: [
-    '/((?!api|_next|static|public|favicon.ico|auth|logo.svg|form/:path*).*)'
+    '/((?!api|_next|static|public|favicon.ico|auth|logo.svg|form).*)'
   ],
 }
